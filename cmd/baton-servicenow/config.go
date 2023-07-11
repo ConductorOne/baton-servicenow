758f3ca22aa0fe047a17ea68680ad617479db725
Martin Albert <martin.albert.187@gmail.com>
2023-07-11 20:53:40 +0200

    Prepare CLI

--- cmd/baton-servicenow/config.go
+++ cmd/baton-servicenow/config.go
@@ -2,16 +2,40 @@ package main
 
 import (
 	"context"
+	"fmt"
 
 	"github.com/conductorone/baton-sdk/pkg/cli"
+	"github.com/spf13/cobra"
 )
 
 // config defines the external configuration required for the connector to run.
 type config struct {
 	cli.BaseConfig `mapstructure:",squash"` // Puts the base config options in the same place as the connector options
+
+	Username string `mapstructure:"username"`
+	Password string `mapstructure:"password"`
+
+	Deployment string `mapstructure:"deployment"`
 }
 
 // validateConfig is run after the configuration is loaded, and should return an error if it isn't valid.
 func validateConfig(ctx context.Context, cfg *config) error {
+	basicNotSet := (cfg.Username == "" || cfg.Password == "")
+
+	if basicNotSet {
+		return fmt.Errorf("username and password must be provided")
+	}
+
+	if cfg.Deployment == "" {
+		return fmt.Errorf("deployment must be provided")
+	}
+
 	return nil
+}
+
+// cmdFlags sets the cmdFlags required for the connector.
+func cmdFlags(cmd *cobra.Command) {
+	cmd.PersistentFlags().String("username", "", "Username of administrator used to connect to the ServiceNow API. ($BATON_USERNAME)")
+	cmd.PersistentFlags().String("password", "", "Application password used to connect to the ServiceNow API. ($BATON_PASSWORD)")
+	cmd.PersistentFlags().String("deployment", "", "ServiceNow deployment to connect to. ($BATON_DEPLOYMENT)")
 }
--- cmd/baton-servicenow/main.go
+++ cmd/baton-servicenow/main.go
@@ -2,11 +2,13 @@ package main
 
 import (
 	"context"
+	"encoding/base64"
 	"fmt"
 	"os"
 
+	"github.com/ConductorOne/baton-servicenow/pkg/connector"
 	"github.com/conductorone/baton-sdk/pkg/cli"
-	"github.com/conductorone/baton-sdk/pkg/sdk"
+	"github.com/conductorone/baton-sdk/pkg/connectorbuilder"
 	"github.com/conductorone/baton-sdk/pkg/types"
 	"github.com/grpc-ecosystem/go-grpc-middleware/logging/zap/ctxzap"
 	"go.uber.org/zap"
@@ -18,13 +20,14 @@ func main() {
 	ctx := context.Background()
 
 	cfg := &config{}
-	cmd, err := cli.NewCmd(ctx, "baton-servicenow", cfg, validateConfig, getConnector, run)
+	cmd, err := cli.NewCmd(ctx, "baton-servicenow", cfg, validateConfig, getConnector)
 	if err != nil {
 		fmt.Fprintln(os.Stderr, err.Error())
 		os.Exit(1)
 	}
 
 	cmd.Version = version
+	cmdFlags(cmd)
 
 	err = cmd.Execute()
 	if err != nil {
@@ -33,40 +36,37 @@ func main() {
 	}
 }
 
+func constructAuth(cfg *config) (string, error) {
+	if cfg.Username != "" {
+		credentials := fmt.Sprintf("%s:%s", cfg.Username, cfg.Password)
+		encodedCredentials := base64.StdEncoding.EncodeToString([]byte(credentials))
+
+		return fmt.Sprintf("Basic %s", encodedCredentials), nil
+	}
+
+	return "", fmt.Errorf("invalid config")
+}
+
 func getConnector(ctx context.Context, cfg *config) (types.ConnectorServer, error) {
 	l := ctxzap.Extract(ctx)
 
-	c, err := sdk.NewEmptyConnector()
+	// compose the auth options
+	auth, err := constructAuth(cfg)
+	if err != nil {
+		return nil, err
+	}
+
+	servicenowConnector, err := connector.New(ctx, auth, cfg.Deployment)
+	if err != nil {
+		l.Error("error creating connector", zap.Error(err))
+		return nil, err
+	}
+
+	c, err := connectorbuilder.NewConnector(ctx, servicenowConnector)
 	if err != nil {
 		l.Error("error creating connector", zap.Error(err))
 		return nil, err
 	}
 
 	return c, nil
-}
-
-// run is where the process of syncing with the connector is implemented.
-func run(ctx context.Context, cfg *config) error {
-	l := ctxzap.Extract(ctx)
-
-	c, err := getConnector(ctx, cfg)
-	if err != nil {
-		l.Error("error creating connector", zap.Error(err))
-		return err
-	}
-
-	r, err := sdk.NewConnectorRunner(ctx, c, cfg.C1zPath)
-	if err != nil {
-		l.Error("error creating connector runner", zap.Error(err))
-		return err
-	}
-	defer r.Close()
-
-	err = r.Run(ctx)
-	if err != nil {
-		l.Error("error running connector", zap.Error(err))
-		return err
-	}
-
-	return nil
 }
