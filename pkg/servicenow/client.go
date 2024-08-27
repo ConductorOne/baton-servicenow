fad7cbcc17693c53157112c1861b09dd1df531a5
Lauren Leach <lauren.leach@conductorone.com>
2024-08-27 14:21:18 -0700

    move to if else

--- pkg/servicenow/client.go
+++ pkg/servicenow/client.go
@@ -443,9 +443,7 @@ func (c *Client) doRequest(ctx context.Context, urlAddress string, method string
 
 	if rawResponse.StatusCode < 0 || rawResponse.StatusCode > math.MaxUint32 {
 		return "", errors.New("status code is out of range for uint32")
-	}
-
-	if rawResponse.StatusCode >= 300 {
+	} else if rawResponse.StatusCode >= 300 {
 		return "", status.Error(codes.Code(uint32(rawResponse.StatusCode)), "Request failed")
 	}
 
